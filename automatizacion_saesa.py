7fd0d333d8a6b0230ba2ba8dc1fd3f36ca6ed71b
Nlorenzenl <nicolas.adev@outlook.com>
2026-06-02 18:25:18 -0400

    Update automatizacion_saesa.py

--- automatizacion_saesa.py
+++ automatizacion_saesa.py
@@ -26,7 +26,7 @@ OPAT_PASS  = os.environ["OPAT_PASS"]
 GMAIL_USER = os.environ["GMAIL_USER"]
 GMAIL_PASS = os.environ["GMAIL_APP_PASS"]
 EMAIL_DEST = os.environ["EMAIL_DEST"]
-EMAIL_CC   = ["alexis.aedo@saesa.cl", "jorge.canete@saesa.cl"]
+EMAIL_CC   = ["nicolas.lorenzen@saesa.cl"]
 
 DRY_RUN          = os.environ.get("DRY_RUN", "true").lower() == "true"
 MAX_APROBACIONES = int(os.environ.get("MAX_APROBACIONES", "50"))
