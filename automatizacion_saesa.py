7a3a06b1c5aa368d2d99993d2fb7701237c5aa44
Nlorenzenl <nicolas.adev@outlook.com>
2026-06-06 13:21:20 -0400

    Update automatizacion_saesa.py

--- automatizacion_saesa.py
+++ automatizacion_saesa.py
@@ -33,7 +33,7 @@ EMAIL_DEST = os.environ["EMAIL_DEST"]
 EMAIL_CC   = ["alexis.aedo@saesa.cl", "jorge.canete@saesa.cl"]
 
 DRY_RUN          = os.environ.get("DRY_RUN", "true").lower() == "true"
-MAX_APROBACIONES = int(os.environ.get("MAX_APROBACIONES", "50"))
+MAX_APROBACIONES = int(os.environ.get("MAX_APROBACIONES", "1"))
 
 TIMEOUT   = 30_000
 TZ_CHILE  = ZoneInfo("America/Santiago")
